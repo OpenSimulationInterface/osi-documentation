59c2212e5c484f9878c58d2a00f715632cff9260
Viktor Kreschenski <viktor.kreschenski@altran.com>
2019-09-03 17:17:01 +0200

    Add osi-validation docu to sphinx

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('/home/vkreschenski/Documents/Projects/OSIValidation/osi-validation')
+sys.path.append('/home/travis/build/OpenSimulationInterface/osi-validation')
 
 # -- Project information -----------------------------------------------------
 
